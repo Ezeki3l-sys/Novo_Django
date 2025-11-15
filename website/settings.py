a901dfe5066af8e29133f191d6c5e5cc2e56c242
QueijoRalado <felipe.gdelima10@gmail.com>
2025-11-14 21:51:06 -0300

    Tentativa de tirar a sobra do Perfil Mobile

--- website/settings.py
+++ website/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'website/static'),)
 #STATIC_ROOT = os.path.join('static')
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
